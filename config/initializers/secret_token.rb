7878a63a541cdc1cf585065b2307fdeb8cbe7ae1
Benjamin Athlan <benjaminathlan@gmail.com>
2010-09-14 11:55:32 +0200

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasx::Application.config.secret_token = '9364712ac2c6c6e78055ef0cc514f0736e2504ed3e14770fa0ae7bcc22a39b7da6f30d0eb55b13ae9c5273a48d51d646760aded7deb9a961a0895bf23c4cca1d'
