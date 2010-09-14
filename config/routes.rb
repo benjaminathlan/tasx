a6c9236b3eba42c097ce9cd5b91328d5030fb949
Benjamin Athlan <benjaminathlan@gmail.com>
2010-09-14 18:35:05 +0200

    addin root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Tasx::Application.routes.draw do
   resources :tasks
-
+  
+  root :to => 'tasks#index'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
+  # match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
